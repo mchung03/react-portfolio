493a566946fd68997f84fd1c6be2d33e793a4176
mchung03 <minhee.chung14@gmail.com>
2023-08-30 19:23:54 -0700

    navtabs bootstrap style and handlepagechange event listeners done

---
+++ src/components/NavTabs.jsx
@@ -0,0 +1,46 @@
+function NavTabs({ currentPage, handlePageChange }) {
+  	return (
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a 
+                    href="#home"
+                    onClick={() => handlePageChange('Home')}
+                    className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a 
+                    href="#about"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                    About
+                </a>
+            </li>
+            <li class="nav-item">
+                <a 
+                    href="#contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact
+                </a>
+            </li>
+            <li class="nav-item">
+                <a 
+                    href="#portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                    Portfolio
+                </a>
+            </li>
+            <li class="nav-item">
+                <a 
+                    href="#resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                    Resume
+                </a>
+            </li>
+        </ul>
+    );
+}
