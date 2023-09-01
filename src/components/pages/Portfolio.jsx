9e62956f9c76aafcf8563fa4fd94b66d12d46ebc
mchung03 <minhee.chung14@gmail.com>
2023-09-01 00:28:30 -0700

    added website screenshots in portfolio.jsx

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -6,6 +6,7 @@ export default function Portfolio() {
 			<div className="row row-cols-2 g-4 mt-3 mb-4">
 				<div className="col">
 				<div className="card">
+					<img src="../../images/reel.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">Reel Reviews</h5>
 					<p className="card-text">Fetching data from the OMDB API, search for your favorite movies and create reviews with an authorized account!</p>
@@ -16,6 +17,7 @@ export default function Portfolio() {
 				</div>
 				<div className="col">
 				<div className="card">
+					<img src="../../images/catfish.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">CatFish</h5>
 					<p className="card-text">Made with HTML, CSS, JavaScript, and APIs, fish animal crossing fish to buy cat facts from the store!</p>
@@ -29,6 +31,7 @@ export default function Portfolio() {
 			<div className="row row-cols-2 g-4 mb-4">
 				<div className="col">
 				<div className="card">
+					<img src="../../images/quiz.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">Coding Quiz</h5>
 					<p className="card-text">Made with HTML, CSS, JavaScript, take this 5 question quiz on coding topics and add your initials to the leaderboard!</p>
@@ -39,11 +42,12 @@ export default function Portfolio() {
 				</div>
 				<div className="col">
 				<div className="card">
+					<img src="../../images/weather.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">Weather Forecast</h5>
 					<p className="card-text">Using the OpenWeather API, search for a city to find out the current and 5 day forecast!</p>
-					<a href="https://mchung03.github.io/weather-forecast-api/Links" target="_blank" className="card-link">Website</a>
-					<a href="https://github.com/mchung03/weather-forecast-api.gitLinks" target="_blank" className="card-link">GitHub</a>
+					<a href="https://mchung03.github.io/weather-forecast-api/" target="_blank" className="card-link">Website</a>
+					<a href="https://github.com/mchung03/weather-forecast-api.git" target="_blank" className="card-link">GitHub</a>
 					</div>
 				</div>
 				</div>
@@ -52,6 +56,7 @@ export default function Portfolio() {
 			<div className="row row-cols-2 g-4">
 				<div className="col">
 				<div className="card">
+					<img src="../../images/note.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">Note Taker</h5>
 					<p className="card-text">Using express.js for routes, users can have a place to write down thoughts, memos, to-dos that will be saved with a click of a button.</p>
@@ -62,6 +67,7 @@ export default function Portfolio() {
 				</div>
 				<div className="col">
 				<div className="card">
+					<img src="../../images/calendar.PNG" class="card-img-top" alt="..."></img>
 					<div className="card-body">
 					<h5 className="card-title">Calendar</h5>
 					<p className="card-text">Using JavaScript, this calendar saves your to-dos by the hour and the color changes based on the time of day!</p>
