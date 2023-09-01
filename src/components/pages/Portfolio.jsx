aea3de3af793043e53a9db9c2c3c392a886de237
mchung03 <minhee.chung14@gmail.com>
2023-08-31 22:25:26 -0700

    completed portfolio.jsx with full bootstrap styling

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,54 +1,76 @@
 export default function Portfolio() {
 	return(
 		<div>
-			<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+			<div class="container">
+			<div class="row row-cols-2 g-4 mt-3 mb-4">
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">Reel Reviews</h5>
 					<p class="card-text">Fetching data from the OMDB API, search for your favorite movies and create reviews with an authorized account!</p>
-					<a href="https://cinemaserver-7dafae9cd971.herokuapp.com/" class="card-link">Website</a>
-					<a href="https://github.com/mattschneble/ReelReviews.git" class="card-link">GitHub</a>
+					<a href="https://cinemaserver-7dafae9cd971.herokuapp.com/" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/mattschneble/ReelReviews.git" target="_blank" class="card-link">GitHub</a>
+					</div>
 				</div>
-				<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+				</div>
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">CatFish</h5>
 					<p class="card-text">Made with HTML, CSS, JavaScript, and APIs, fish animal crossing fish to buy cat facts from the store!</p>
-					<a href="https://kaylaasana.github.io/CatFish/" class="card-link">Website</a>
-					<a href="https://github.com/kaylaasana/CatFish.git" class="card-link">GitHub</a>
+					<a href="https://kaylaasana.github.io/CatFish/" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/kaylaasana/CatFish.git" target="_blank" class="card-link">GitHub</a>
+					</div>
+				</div>
 				</div>
 			</div>
-			<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+			
+			<div class="row row-cols-2 g-4 mb-4">
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">Coding Quiz</h5>
 					<p class="card-text">Made with HTML, CSS, JavaScript, take this 5 question quiz on coding topics and add your initials to the leaderboard!</p>
-					<a href="https://mchung03.github.io/coding-timer-quiz/" class="card-link">Website</a>
-					<a href="https://github.com/mchung03/coding-timer-quiz" class="card-link">GitHub</a>
+					<a href="https://mchung03.github.io/coding-timer-quiz/" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/mchung03/coding-timer-quiz" target="_blank" class="card-link">GitHub</a>
+					</div>
 				</div>
-			</div>
-			<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+				</div>
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">Weather Forecast</h5>
 					<p class="card-text">Using the OpenWeather API, search for a city to find out the current and 5 day forecast!</p>
-					<a href="https://mchung03.github.io/weather-forecast-api/Links" class="card-link">Website</a>
-					<a href="https://github.com/mchung03/weather-forecast-api.gitLinks" class="card-link">GitHub</a>
+					<a href="https://mchung03.github.io/weather-forecast-api/Links" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/mchung03/weather-forecast-api.gitLinks" target="_blank" class="card-link">GitHub</a>
+					</div>
+				</div>
 				</div>
 			</div>
-			<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+			
+			<div class="row row-cols-2 g-4">
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">Note Taker</h5>
 					<p class="card-text">Using express.js for routes, users can have a place to write down thoughts, memos, to-dos that will be saved with a click of a button.</p>
-					<a href="https://limitless-meadow-93070-8ba2c454b807.herokuapp.com/" class="card-link">Website</a>
-					<a href="https://github.com/mchung03/note-taker-app.git" class="card-link">GitHub</a>
+					<a href="https://limitless-meadow-93070-8ba2c454b807.herokuapp.com/" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/mchung03/note-taker-app.git" target="_blank" class="card-link">GitHub</a>
+					</div>
 				</div>
-			</div>
-			<div class="card" style={{ width: '18rem' }}>
-				<div class="card-body">
+				</div>
+				<div class="col">
+				<div class="card">
+					<div class="card-body">
 					<h5 class="card-title">Calendar</h5>
 					<p class="card-text">Using JavaScript, this calendar saves your to-dos by the hour and the color changes based on the time of day!</p>
-					<a href="https://mchung03.github.io/challenge-5-calendar" class="card-link">Website</a>
-					<a href="https://github.com/mchung03/challenge-5-calendar.git" class="card-link">GitHub</a>
+					<a href="https://mchung03.github.io/challenge-5-calendar" target="_blank" class="card-link">Website</a>
+					<a href="https://github.com/mchung03/challenge-5-calendar.git" target="_blank" class="card-link">GitHub</a>
+					</div>
+				</div>
 				</div>
 			</div>
-		</div>
+			</div>
 		</div>
 	)
 }
