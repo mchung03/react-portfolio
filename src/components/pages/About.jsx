2b19ea60c47a7d4b82793e31d55390846a5b8d86
mchung03 <minhee.chung14@gmail.com>
2023-09-06 21:26:34 -0700

    updated biography

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
             <div className="col-md-8 card-color">
             <div className="card-body">
                 <h4 className="card-title fw-semibold">Biography</h4>
-                <p className="card-text">Minhee Chung is a student at the UC Berkeley Extension Fullstack Coding Bootcamp. She is also currently a student at the University of California, Riverside studying Business Administration, as she aspires to create a small web developing company after gaining more experience in the coding field.</p>
+                <p className="card-text">Minhee Chung is a student at the UC Berkeley Extension Fullstack Coding Bootcamp. She is also currently a student at the University of California, Riverside studying Business Administration with a concentration in Information Systems and hopes to gain more experience in the coding field.</p>
             </div>
             </div>
         </div>
